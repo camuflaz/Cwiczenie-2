f35fe032a86e23d7b266194975ae0b746cd15a74
camuflaz <73291870+camuflaz@users.noreply.github.com>
2024-03-11 13:24:03 +0000

    Commit_1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,61 @@
 // import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // import org.junit.jupiter.api.Test;
-
+import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World!!!");
-  }
+    // Hello World
+    //System.out.println("Hello World!!!");
+    
+    // Wypisywanie zmiennej
+    /* int zmienna = 1;
+    System.out.println(zmienna);
+    */
+    
+    // Wypisywanie sumy 
+    /* int x = 5;
+    int y = 9;
+    System.out.println(x + y);
+    */
 
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
+    // Kalkulator
+    /* Scanner scan = new Scanner(System.in);
+    System.out.println("Wybierz działanie: 1 - Suma 2 - Różnica 3 - Iloczyn 4 - Iloraz");
+    int wybor = scan.nextInt();
+    System.out.println("Podaj pierwszą liczbę: ");
+    double x = scan.nextDouble();
+    System.out.println("Podaj drugą liczbę: ");
+    double y = scan.nextDouble();
+
+    if(wybor==1){
+      System.out.println(x+y);
+    }
+    else if(wybor==2){
+      System.out.println(x-y);
+    }
+    else if(wybor==3){
+      System.out.println(x*y);
+    }
+    else if(wybor==4){
+      if(y==0){
+        System.out.println("Nie dzielimy przez 0");
+      }
+      System.out.println(x/y);
+    }
+    else{
+      System.out.println("Nie ma takiej opcji");
+    }
+    */
+
+    // Liczby parzyste
+    /*for(int i = 0; i <= 100; i++){
+      if(i%2==0){
+        System.out.println(i);
+      }
+    }
+    */
+    
+    
+  }
+  
 }
